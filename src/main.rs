f2a9c6650ada53d5e809cd0d39b3ba5105ae0420
Dan Davison <dandavison7@gmail.com>
2019-06-23 21:29:51 -0400

    Handle error conditions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate unidiff;
 
-use std::io::{self, Read, Write};
+use std::io::{self, Read};
 use std::path::Path;
 
 use syntect::easy::HighlightLines;
@@ -14,20 +14,35 @@ fn main() {
     let ts = ThemeSet::load_defaults();
 
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input);
+    io::stdin().read_to_string(&mut input).expect("Error reading input");
     let mut patch_set = PatchSet::new();
-    patch_set.parse(&mut input).ok().expect("Error parsing diff");
+    patch_set.parse(&mut input).ok().expect("Error parsing input as a diff");
     for patched_file in patch_set {
         let path = Path::new(&patched_file.source_file);
-        let syntax = ps.find_syntax_by_extension(path.extension().unwrap().to_str().unwrap()).unwrap();
-        let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
-        io::stdout().write_all(format!("\n<<<<<<<<<<<<<<<<<<<<<< file: {}\n", patched_file.source_file).as_bytes());
-        for hunk in patched_file {
-            for line in LinesWithEndings::from(&hunk.to_string()) {
-                let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
-                let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
-                println!("{}", escaped);
+        let extension = path.extension()
+            .expect(format!("Error determining file type: {}", path.to_str().unwrap()).as_str());
+        let extension_str = extension.to_str().unwrap();
+        let syntax = ps.find_syntax_by_extension(extension_str);
+
+        match syntax {
+            Some(syntax) => {
+                let mut highlighter = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+                for hunk in patched_file {
+                    for line in LinesWithEndings::from(&hunk.to_string()) {
+                        let ranges: Vec<(Style, &str)> = highlighter.highlight(line, &ps);
+                        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+                        print!("{}", escaped);
+                    }
+                }
+            },
+            None => {
+                for hunk in patched_file {
+                    for line in LinesWithEndings::from(&hunk.to_string()) {
+                        print!("{}", line);
+                    }
+                }
             }
         }
     }
+    println!("");
 }
