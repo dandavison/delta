cf4751aabc26bdb9dbbbbd47a872b4bd94911ec9
Dan Davison <dandavison7@gmail.com>
2019-06-25 11:36:57 -0400

    DNC: simplify diff parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-extern crate unidiff;
-
 use std::fmt::Write;
-use std::io::{self, Read};
+use std::io::{self, BufRead};
 use std::path::Path;
 
 use syntect::easy::HighlightLines;
 use syntect::highlighting::{Color, Style, ThemeSet};
 use syntect::parsing::SyntaxSet;
-use syntect::util::LinesWithEndings;
-use unidiff::PatchSet;
 
 pub const DELTA_THEME_DEFAULT: &str = "InspiredGitHub"; // base16-mocha.dark
 
@@ -26,66 +22,58 @@ const RED: Color = Color {
     a: 0x00,
 };
 
+#[derive(PartialEq)]
+enum State {
+    Commit,
+    DiffMeta,
+    DiffHunk,
+    Unknown,
+}
+
 fn main() {
     let ps = SyntaxSet::load_defaults_newlines();
     let ts = ThemeSet::load_defaults();
     let theme = &ts.themes[DELTA_THEME_DEFAULT];
-    let mut input = String::new();
     let mut output = String::new();
 
-    io::stdin().read_to_string(&mut input).expect(
-        "Error reading input",
-    );
-    let mut patch_set = PatchSet::new();
-    patch_set.parse(&mut input).ok().expect(
-        "Error parsing input as a diff",
-    );
-    for patched_file in patch_set {
-        // TODO: use both source and target to determine language
-        let path = Path::new(&patched_file.target_file);
-        let extension =
-            path.extension().expect(
-                format!("Error determining file type: {}", path.to_str().unwrap())
-                    .as_str(),
-            );
-        let extension_str = extension.to_str().unwrap();
-        let syntax = ps.find_syntax_by_extension(extension_str);
+    let mut state = State::Unknown;
+    let mut highlighter: Option<HighlightLines> = None;
 
-        print!(
-            "\n{}\n{}\n\n",
-            "__________________________________________________________________",
-            path.display(),
-        );
-
-        match syntax {
-            Some(syntax) => {
-                let mut highlighter = HighlightLines::new(syntax, theme);
-                for hunk in patched_file {
-                    for line in LinesWithEndings::from(&hunk.to_string()) {
-                        let background_color = match line.chars().next() {
-                            Some('+') => Some(GREEN),
-                            Some('-') => Some(RED),
-                            _ => None,
-                        };
-                        let ranges: Vec<(Style, &str)> = highlighter.highlight(line, &ps);
-                        my_as_24_bit_terminal_escaped(&ranges[..], background_color, &mut output);
-                        print!("{}", output);
-                        output.truncate(0);
-                    }
+    for _line in io::stdin().lock().lines() {
+        let line = _line.unwrap();
+        if line.starts_with("diff --") {
+            state = State::DiffMeta;
+            let extension = get_file_extension_from_diff_line(&line);
+            highlighter = match ps.find_syntax_by_extension(extension) {
+                Some(syntax) => Some(HighlightLines::new(syntax, theme)),
+                None => None,
+            }
+        } else if line.starts_with("commit") {
+            state = State::Commit;
+        } else if line.starts_with("@@") {
+            state = State::DiffHunk;
+        } else if state == State::DiffHunk {
+            match highlighter {
+                Some(mut highlighter) => {
+                    let background_color = match line.chars().next() {
+                        Some('+') => Some(GREEN),
+                        Some('-') => Some(RED),
+                        _ => None,
+                    };
+                    let ranges: Vec<(Style, &str)> = highlighter.highlight(&line, &ps);
+                    my_as_24_bit_terminal_escaped(&ranges[..], background_color, &mut output);
+                    print!("{}", output);
+                    output.truncate(0);
+                }
+                None => {
+                    print!("{}", line);
                 }
             }
-            None => {
-                for hunk in patched_file {
-                    for line in LinesWithEndings::from(&hunk.to_string()) {
-                        print!("{}", line);
-                    }
-                }
-            }
-        }
+        };
     }
-    println!("");
 }
 
+/// Based on as_24_bit_terminal_escaped from syntect
 fn my_as_24_bit_terminal_escaped(
     v: &[(Style, &str)],
     background_color: Option<Color>,
@@ -114,4 +102,17 @@ fn my_as_24_bit_terminal_escaped(
         ).unwrap();
     }
     buf.push_str("\x1b[0m");
+}
+
+
+
+fn get_file_extension_from_diff_line(line: &String) -> &str {
+
+    //let (old, new) = get_file_names_from_diff_line(&line);
+    let path = Path::new(line);
+    let extension =
+        path.extension().expect(
+            format!("Error determining file type: {}", path.to_str().unwrap()).as_str(),
+        );
+    extension.to_str().unwrap()
 }
