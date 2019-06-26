a81c86cc4cf42f23ea2ef83459e2a54933ccdda1
Dan Davison <dandavison7@gmail.com>
2019-06-25 22:52:56 -0400

    Parse file names and extensions

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,10 @@ fn main() {
         let line = _line.unwrap();
         if line.starts_with("diff --") {
             state = State::DiffMeta;
-            let extension = get_file_extension_from_diff_line(&line);
-            syntax = ps.find_syntax_by_extension(extension);
+            syntax = match get_file_extension_from_diff_line(&line) {
+                Some(extension) => ps.find_syntax_by_extension(extension),
+                None => None,
+            };
         } else if line.starts_with("commit") {
             state = State::Commit;
         } else if line.starts_with("@@") {
@@ -99,16 +101,42 @@ fn my_as_24_bit_terminal_escaped(
     buf.push_str("\x1b[0m");
 }
 
+/// Given input like
+/// "diff --git a/src/main.rs b/src/main.rs"
+/// Return "rs", i.e. a single file extension consistent with both files.
+fn get_file_extension_from_diff_line(line: &str) -> Option<&str> {
+    match get_file_extensions_from_diff_line(line) {
+        (Some(ext1), Some(ext2)) => {
+            if ext1 == ext2 {
+                Some(ext1)
+            } else {
+                // TODO: Just return None and output without color.
+                panic!(
+                    "Old and new files have different extensions: {} vs {}",
+                    ext1,
+                    ext2
+                );
+            }
+        }
+        (Some(ext1), None) => Some(ext1),
+        (None, Some(ext2)) => Some(ext2),
+        (None, None) => None,
+    }
+}
 
+/// Given input like "diff --git a/src/main.rs b/src/main.rs"
+/// return ("src/main.rs", "src/main.rs").
+fn get_file_extensions_from_diff_line(line: &str) -> (Option<&str>, Option<&str>) {
+    let mut iter = line.split(" ");
+    iter.next(); // diff
+    iter.next(); // --git
+    (
+        iter.next().and_then(|s| get_extension(&s[2..])),
+        iter.next().and_then(|s| get_extension(&s[2..])),
+    )
+}
 
-fn get_file_extension_from_diff_line(line: &String) -> &str {
-
-    //let (old, new) = get_file_names_from_diff_line(&line);
-    let path = Path::new(line);
-    let extension =
-        path.extension().expect(
-            format!("Error determining file type: {}", path.to_str().unwrap()).as_str(),
-        );
-    extension.to_str().unwrap();
-    "rs"
+/// Attempt to parse input as a file path and return extension as a &str.
+fn get_extension(s: &str) -> Option<&str> {
+    Path::new(s).extension().and_then(|e| e.to_str())
 }
