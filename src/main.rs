d2fc9f54a9f670c9007e2c5d2b65cf6e59b0cd08
Dan Davison <dandavison7@gmail.com>
2019-07-11 12:19:02 -0400

    Add failing integration test

--- src/main.rs
+++ src/main.rs
@@ -242,3 +242,69 @@ pub fn list_themes() -> std::io::Result<()> {
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use console::strip_ansi_codes;
+
+    #[test]
+    #[ignore]
+    fn test_added_file() {
+        let input = "\
+commit d28dc1ac57e53432567ec5bf19ad49ff90f0f7a5
+Author: Dan Davison <dandavison7@gmail.com>
+Date:   Thu Jul 11 10:41:11 2019 -0400
+
+    .
+
+diff --git a/a.py b/a.py
+new file mode 100644
+index 0000000..8c55b7d
+--- /dev/null
++++ b/a.py
+@@ -0,0 +1,3 @@
++# hello
++class X:
++    pass";
+
+        let expected_output = "\
+commit d28dc1ac57e53432567ec5bf19ad49ff90f0f7a5
+Author: Dan Davison <dandavison7@gmail.com>
+Date:   Thu Jul 11 10:41:11 2019 -0400
+
+    .
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+modified: a.py
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+────────────────────────────────────────────────────────────────────────────────
+
+────────────────────────────────────────────────────────────────────────────────
+ # hello
+ class X:
+     pass
+";
+
+        let mut opt = Opt::from_args();
+        opt.width = Some("variable".to_string());
+        let assets = HighlightingAssets::new();
+        let paint_config = process_command_line_arguments(&assets, &opt);
+        let mut writer: Vec<u8> = Vec::new();
+        delta(
+            input.split("\n").map(String::from),
+            &paint_config,
+            &assets,
+            &mut writer,
+        )
+        .unwrap();
+        let output = strip_ansi_codes(&String::from_utf8(writer).unwrap()).to_string();
+        assert!(output.contains("\nadded: a.py\n"));
+        if false {
+            // TODO: hline width
+            assert_eq!(output, expected_output);
+        }
+    }
+
+}
