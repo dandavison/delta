a782894a76e9087b4bd3d1be1ff82ff9b84f42d3
Dan Davison <dandavison7@gmail.com>
2019-07-16 13:54:35 -0400

    Homebrew formula

--- HomeBrewFormula/git-delta.rb
+++ HomeBrewFormula/git-delta.rb
@@ -5,10 +5,10 @@ class GitDelta < Formula
 
   if OS.mac?
       url "https://github.com/dandavison/delta/releases/download/#{version}/delta-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "453b4652f55f86ee3aeb128e7cda3cf6cdc9fdbeac4c69a76e4c96c88a9af99b"
+      sha256 "32ef2f3c894206ebd11dd5d4a15a7337125f8d58efceca163d27d041ef64042c"
   elsif OS.linux?
       url "https://github.com/dandavison/delta/releases/download/#{version}/delta-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "73ef55027433bb72ae2457428ae1c2807d1f807cfaa111692e58ab4f4e27b8d0"
+      sha256 "f7253dab9e689e96b72ca1b935f84aeee5eb03223217826bd8339ae87f37982e"
   end
 
   conflicts_with "delta"
