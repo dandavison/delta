1ae56f924f6f8d4a0d398ea72e47270f0e1112fb
Dan Davison <dandavison7@gmail.com>
2019-08-08 11:06:49 -0700

    Bump homebrew formula

--- HomeBrewFormula/git-delta.rb
+++ HomeBrewFormula/git-delta.rb
@@ -1,14 +1,14 @@
 class GitDelta < Formula
-  version "0.0.7"
+  version "0.0.8"
   desc "A syntax-highlighting pager for git"
   homepage "https://github.com/dandavison/delta"
 
   if OS.mac?
       url "https://github.com/dandavison/delta/releases/download/#{version}/delta-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "1e9f1878cab0878af3f3f7e68ee612d3beb97811bdcf2acd82c3137ebc180bee"
+      sha256 "5254fd9b0aae158eae4ad9b0ef1725e57e17df68ec27365343bfa9c7ba094a1f"
   elsif OS.linux?
       url "https://github.com/dandavison/delta/releases/download/#{version}/delta-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "783ef27d4c32cba7a1c27ab6d61edebb42a7b3044dee4fd829c38f5574cab8aa"
+      sha256 "2bb95762c6bd43ffbd14ea6bb2f2d95c7ecbf97f8af64fa39db81458cc93ff6c"
   end
 
   conflicts_with "delta"
