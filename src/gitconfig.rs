fbfe6d9db246f7757cc7b6cb941a868fc46b652b
Dan Davison <dandavison7@gmail.com>
2020-06-12 14:11:42 -0400

    Add failing test of diff-so-fancy preset precedence

--- src/gitconfig.rs
+++ src/gitconfig.rs
@@ -526,6 +526,59 @@ mod tests {
         remove_file(git_config_path).unwrap();
     }
 
+    #[test]
+    fn test_diff_so_fancy_obeys_preset_precedence_rules() {
+        let git_config_contents = b"
+[color \"diff\"]
+    meta = 11
+    frag = magenta bold
+    commit = yellow bold
+    old = red bold
+    new = green bold
+    whitespace = red reverse
+
+[delta \"decorations\"]
+    commit-decoration-style = bold box ul
+    file-style = bold 19 ul
+    file-decoration-style = none
+";
+        let git_config_path = "delta__test_diff_so_fancy_obeys_preset_precedence_rules.gitconfig";
+
+        let config = make_config(
+            &["--presets", "decorations diff-so-fancy"],
+            Some(git_config_contents),
+            Some(git_config_path),
+        );
+
+        assert_eq!(
+            config.file_style.ansi_term_style,
+            make_style("11").ansi_term_style
+        );
+
+        assert_eq!(
+            config.file_style.decoration_style,
+            make_decoration_style("yellow bold ul ol")
+        );
+
+        let config = make_config(
+            &["--presets", "diff-so-fancy decorations"],
+            Some(git_config_contents),
+            Some(git_config_path),
+        );
+
+        assert_eq!(
+            config.file_style.ansi_term_style,
+            make_style("ul bold 19").ansi_term_style
+        );
+
+        assert_eq!(
+            config.file_style.decoration_style,
+            make_decoration_style("none")
+        );
+
+        remove_file(git_config_path).unwrap();
+    }
+
     fn make_style(s: &str) -> Style {
         _make_style(s, false)
     }
