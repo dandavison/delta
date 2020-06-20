3aaa9606e6a64e8dd16fb46520da01a8186367da
Dan Davison <dandavison7@gmail.com>
2020-06-20 14:05:33 -0400

    Revert "Hack: allow file-modified-label to be overridden in gitconfig"
    This reverts commit 574e5d2ea05940e56364aadcf8b5b5f0344bd01c.

--- src/rewrite_options.rs
+++ src/rewrite_options.rs
@@ -35,11 +35,7 @@ fn rewrite_options_to_implement_color_only(opt: &mut cli::Opt) {
 /// Implement --navigate
 fn rewrite_options_to_implement_navigate(opt: &mut cli::Opt, arg_matches: &clap::ArgMatches) {
     if opt.navigate {
-        // Hack: perhaps navigate should be a built-in feature
-        let default_value = "";
-        if !user_supplied_option("file-modified-label", arg_matches)
-            && opt.file_modified_label == default_value
-        {
+        if !user_supplied_option("file-modified-label", arg_matches) {
             opt.file_modified_label = "Δ".to_string();
         }
     }
