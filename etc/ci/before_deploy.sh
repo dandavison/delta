955007a1f0e2f244ca8af19b7eaab8b478922b02
chris48s <chris.shaw480@gmail.com>
2020-08-23 22:18:22 +0100

    include years in copyright notice
    This brings the copyright notice into line with the expected format
    resolves copyright-without-copyright-notice
    https://lintian.debian.org/tags/copyright-without-copyright-notice.html

--- etc/ci/before_deploy.sh
+++ etc/ci/before_deploy.sh
@@ -61,6 +61,7 @@ make_deb() {
 
     homepage="https://github.com/dandavison/delta"
     maintainer="Dan Davison <dandavison7@gmail.com>"
+    copyright_years="2019 - "$(date "+%Y")
 
     case $TARGET in
         x86_64*)
@@ -118,7 +119,7 @@ Upstream-Name: $PROJECT_NAME
 Source: $homepage
 
 Files: *
-Copyright: $maintainer
+Copyright: $copyright_years $maintainer
 License: MIT
 
 License: MIT
