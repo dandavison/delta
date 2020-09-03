344355e02876629c6b6fb908c564ad844e497ab9
Dan Davison <dandavison7@gmail.com>
2020-09-03 09:49:22 -0400

    Change variable name

--- src/env.rs
+++ src/env.rs
@@ -1,9 +1,9 @@
 use std::env;
 
-/// If key is set and, after trimming whitespace, is not empty string, then return that trimmed
+/// If `name` is set and, after trimming whitespace, is not empty string, then return that trimmed
 /// string. Else None.
-pub fn get_env_var(key: &str) -> Option<String> {
-    match env::var(key).unwrap_or("".to_string()).trim() {
+pub fn get_env_var(name: &str) -> Option<String> {
+    match env::var(name).unwrap_or("".to_string()).trim() {
         "" => None,
         non_empty_string => Some(non_empty_string.to_string()),
     }
