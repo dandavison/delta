3611987ce502cf88f2b29a5ea557367732c3e881
Michał Kiełbowicz <mpkielbowicz@gmail.com>
2020-10-28 16:14:44 +0100

    Add gruvbox light support

--- src/options/theme.rs
+++ src/options/theme.rs
@@ -38,8 +38,10 @@ pub fn is_light_syntax_theme(theme: &str) -> bool {
     LIGHT_SYNTAX_THEMES.contains(&theme)
 }
 
-const LIGHT_SYNTAX_THEMES: [&str; 5] = [
+const LIGHT_SYNTAX_THEMES: [&str; 7] = [
     "GitHub",
+    "gruvbox-light",
+    "gruvbox-white",
     "Monokai Extended Light",
     "OneHalfLight",
     "ansi-light",
