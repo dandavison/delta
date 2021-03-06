d10412eba2f4cfb99a61b99692fe2c590ba3b528
Dan Davison <dandavison7@gmail.com>
2021-03-05 19:10:33 -0500

    Store delta less hist file in XDG data directory

--- src/features/navigate.rs
+++ src/features/navigate.rs
@@ -1,5 +1,4 @@
 use std::io::Write;
-use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
 
 use crate::config::Config;
@@ -42,27 +41,25 @@ fn make_navigate_regexp(config: &Config) -> String {
 // history file so, for example, a (non-navigate) search performed in the delta less process will
 // not be stored in history.
 pub fn copy_less_hist_file_and_append_navigate_regexp(config: &Config) -> std::io::Result<PathBuf> {
-    let home_dir = dirs_next::home_dir()
-        .ok_or_else(|| Error::new(ErrorKind::NotFound, "Can't determine home dir"))?;
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("delta")?;
+    let delta_less_hist = xdg_dirs.place_data_file("lesshst")?;
     let initial_contents = "\
 .less-history-file:
 .search
 "
     .to_string();
-    let contents = if let Some(hist_file) = get_less_hist_file(&home_dir) {
+    let contents = if let Some(hist_file) = get_less_hist_file() {
         std::fs::read_to_string(hist_file).unwrap_or(initial_contents)
     } else {
         initial_contents
     };
-    let mut hist_file = home_dir;
-    hist_file.push(".delta.lesshst");
     writeln!(
-        std::fs::File::create(&hist_file)?,
+        std::fs::File::create(&delta_less_hist)?,
         "{}\"{}",
         contents,
         make_navigate_regexp(config)
     )?;
-    Ok(hist_file)
+    Ok(delta_less_hist)
 }
 
 // LESSHISTFILE
@@ -73,26 +70,30 @@ pub fn copy_less_hist_file_and_append_navigate_regexp(config: &Config) -> std::i
 //        "$HOME/_lesshst" on DOS and Windows systems, or
 //        "$HOME/lesshst.ini" or "$INIT/lesshst.ini" on OS/2
 //        systems.
-fn get_less_hist_file(home_dir: &PathBuf) -> Option<PathBuf> {
-    match std::env::var("LESSHISTFILE").as_deref() {
-        Ok("-") | Ok("/dev/null") => {
-            // The user has explicitly disabled less history.
-            None
+fn get_less_hist_file() -> Option<PathBuf> {
+    if let Some(home_dir) = dirs_next::home_dir() {
+        match std::env::var("LESSHISTFILE").as_deref() {
+            Ok("-") | Ok("/dev/null") => {
+                // The user has explicitly disabled less history.
+                None
+            }
+            Ok(path) => {
+                // The user has specified a custom histfile
+                Some(PathBuf::from(path))
+            }
+            Err(_) => {
+                // The user is using the default less histfile location.
+                let mut hist_file = home_dir.clone();
+                hist_file.push(if cfg!(windows) {
+                    "_lesshst"
+                } else {
+                    ".lesshst"
+                });
+                Some(hist_file)
+            }
         }
-        Ok(path) => {
-            // The user has specified a custom histfile
-            Some(PathBuf::from(path))
-        }
-        Err(_) => {
-            // The user is using the default less histfile location.
-            let mut hist_file = home_dir.clone();
-            hist_file.push(if cfg!(windows) {
-                "_lesshst"
-            } else {
-                ".lesshst"
-            });
-            Some(hist_file)
-        }
+    } else {
+        None
     }
 }
 
