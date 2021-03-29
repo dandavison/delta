e1ef7f54ce0b053a12155fd493379d4d68a91ec1
Dan Davison <dandavison7@gmail.com>
2021-03-29 19:01:51 -0400

    Bump version in private Homebrew formula

--- HomeBrewFormula/git-delta.rb
+++ HomeBrewFormula/git-delta.rb
@@ -7,10 +7,10 @@ class GitDelta < Formula
 
   if OS.mac?
       url "https://github.com/dandavison/delta/releases/download/#{version}/delta-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "4124b38e7bbd73259c8119e9be5cbe8d4de9dfcbd9fac8f375466b185536d8b3"
+      sha256 "d6368d3004fd39f4d1665a8fb91867d8e96f5ed59ef271925c41de6090fe44cb"
   elsif OS.linux?
       url "https://github.com/dandavison/delta/releases/download/#{version}/delta-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "20b59a1c0b74c0f0c0d3c72d7ddbfa857f5c49dd281b84d080263aa5d9da496a"
+      sha256 "7c5ddf43cb3b0f0640654df00ea31bf509bc364827793b93d0d5600ead0704beg"
   end
 
   conflicts_with "delta"
