479ed8c1fbd1df531406dfac2d833460c43bd9ac
Dan Davison <dandavison7@gmail.com>
2021-08-21 18:21:22 -0700

    Improve help text: slowness is due to edit inference
    Ref #659

--- src/cli.rs
+++ src/cli.rs
@@ -496,8 +496,7 @@ pub struct Opt {
 
     #[structopt(long = "max-line-length", default_value = "512")]
     /// Truncate lines longer than this. To prevent any truncation, set to zero. Note that
-    /// syntax-highlighting very long lines (e.g. minified .js) will be very slow if they are not
-    /// truncated.
+    /// delta will be slow on very long lines (e.g. minified .js) if truncation is disabled.
     pub max_line_length: usize,
 
     /// The width of underline/overline decorations. Use --width=variable to extend decorations and
