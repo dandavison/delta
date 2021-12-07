47b073d224757217de468063904d1c8342e290d1
Thomas Otto <th1000s@posteo.net>
2021-12-07 22:42:34 +0100

    Move explain_ansi() part out of parse_ansi()

--- src/subcommands/parse_ansi.rs
+++ src/subcommands/parse_ansi.rs
@@ -2,19 +2,16 @@ use std::io::{self, BufRead};
 
 #[cfg(not(tarpaulin_include))]
 pub fn parse_ansi() -> std::io::Result<()> {
-    use crate::{ansi, style::Style};
+    use crate::ansi;
 
     for line in io::stdin().lock().lines() {
-        for (ansi_term_style, s) in ansi::parse_style_sections(
-            &line.unwrap_or_else(|line| panic!("Invalid utf-8: {:?}", line)),
-        ) {
-            let style = Style {
-                ansi_term_style,
-                ..Style::default()
-            };
-            print!("({}){}", style.to_painted_string(), style.paint(s));
-        }
-        println!();
+        println!(
+            "{}",
+            ansi::explain_ansi(
+                &line.unwrap_or_else(|line| panic!("Invalid utf-8: {:?}", line)),
+                true
+            )
+        );
     }
     Ok(())
 }
