e524ababef4d75d877e58229d4c18af238661c54
Dan Davison <dandavison7@gmail.com>
2021-12-12 08:16:19 -0500

    Avoid using environment variables during tests

--- src/env.rs
+++ src/env.rs
@@ -1,15 +1,24 @@
+#[cfg(not(test))]
 use std::env;
 
 /// If `name` is set and, after trimming whitespace, is not empty string, then return that trimmed
 /// string. Else None.
-pub fn get_env_var(name: &str) -> Option<String> {
-    match env::var(name).unwrap_or_else(|_| "".to_string()).trim() {
+pub fn get_env_var(_name: &str) -> Option<String> {
+    #[cfg(not(test))]
+    match env::var(_name).unwrap_or_else(|_| "".to_string()).trim() {
         "" => None,
         non_empty_string => Some(non_empty_string.to_string()),
     }
+    #[cfg(test)]
+    None
 }
 
 /// If `name` is set to any value at all (including "") then return true; else false.
-pub fn get_boolean_env_var(name: &str) -> bool {
-    env::var(name).ok().is_some()
+pub fn get_boolean_env_var(_name: &str) -> bool {
+    #[cfg(not(test))]
+    {
+        env::var(_name).ok().is_some()
+    }
+    #[cfg(test)]
+    false
 }
