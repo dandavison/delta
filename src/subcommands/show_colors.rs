6208283f6d9cbeb6fda2915afa3a6158d030b01e
Thomas Otto <th1000s@posteo.net>
2023-06-05 22:21:07 +0200

    Wrap --help output and use pager
    Set clap option `max_term_width`, so the help output is by default as
    wide as the terminal or this max value. Then manually wrap the
    `after_long_help()` text (only on demand) to the same width using
    `textwrap`. Also use matching ansi codes in this section.
    
    The help output is now paginated if output is to a terminal.
    
    All code paths flow back to main() so an invoked pager is properly
    waited for (by the OutputType drop impl).

--- src/subcommands/show_colors.rs
+++ src/subcommands/show_colors.rs
@@ -15,7 +15,12 @@ pub fn show_colors() -> std::io::Result<()> {
 
     let assets = utils::bat::assets::load_highlighting_assets();
     let env = DeltaEnv::default();
-    let opt = cli::Opt::from_args_and_git_config(&env, assets);
+
+    let opt = match cli::Opt::from_args_and_git_config(&env, assets) {
+        cli::Call::Delta(opt) => opt,
+        _ => panic!("non-Delta Call variant should not occur here"),
+    };
+
     let config = config::Config::from(opt);
     let pagercfg = (&config).into();
 
