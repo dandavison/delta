dandavison/delta

commit 07e951db2224f94051b6d732f9727d8c2664f01e

Add missing `--file-*-label` option to `--navigate` docs (#1744)
Also sort the options alphabetically

src/cli.rs
@@ -851,7 +851,7 @@ pub struct Opt {
     /// Activate diff navigation.
     ///
     /// Use n to jump forwards and N to jump backwards. To change the file labels used see
-    /// --file-modified-label, --file-removed-label, --file-added-label, --file-renamed-label.
+    /// --file-added-label, --file-copied-label, --file-modified-label, --file-removed-label, , --file-renamed-label.
     pub navigate: bool,
 
     #[arg(long = "navigate-regex", value_name = "REGEX")]

commit 655fc7669dede3859aa3edee66f351405ea860f7

Fix copy-paste typo
Accidentally introduced in #1744

src/cli.rs
@@ -851,7 +851,7 @@ pub struct Opt {
     /// Activate diff navigation.
     ///
     /// Use n to jump forwards and N to jump backwards. To change the file labels used see
-    /// --file-added-label, --file-copied-label, --file-modified-label, --file-removed-label, , --file-renamed-label.
+    /// --file-added-label, --file-copied-label, --file-modified-label, --file-removed-label, --file-renamed-label.
     pub navigate: bool,
 
     #[arg(long = "navigate-regex", value_name = "REGEX")]